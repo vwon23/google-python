34e55f70c16c81cd473431b1dfc81090d41a1f47
vwon23 <vwon87@gmail.com>
2017-05-18 14:33:36 -0700

    Corrected list1.py
    Accidentally pasted string1.py solution

--- basic/list1.py
+++ basic/list1.py
@@ -6,116 +6,85 @@
 # Google's Python Class
 # http://code.google.com/edu/languages/google-python-class/
 
-# Basic string exercises
+# Basic list exercises
 # Fill in the code for the functions below. main() is already set up
 # to call the functions with a few different inputs,
 # printing 'OK' when each function is correct.
 # The starter code for each function includes a 'return'
 # which is just a placeholder for your code.
 # It's ok if you do not complete all the functions, and there
-# are some additional functions to try in string2.py.
+# are some additional functions to try in list2.py.
 
-
-# A. donuts
-# Given an int count of a number of donuts, return a string
-# of the form 'Number of donuts: <count>', where <count> is the number
-# passed in. However, if the count is 10 or more, then use the word 'many'
-# instead of the actual count.
-# So donuts(5) returns 'Number of donuts: 5'
-# and donuts(23) returns 'Number of donuts: many'
-def donuts(count):
+# A. match_ends
+# Given a list of strings, return the count of the number of
+# strings where the string length is 2 or more and the first
+# and last chars of the string are the same.
+# Note: python does not have a ++ operator, but += works.
+def match_ends(words):
   # +++your code here+++
-  if count < 10:
-    return 'Number of donuts: %d' %(count)
-  else:
-    return 'Number of donuts: many'
+  return
 
 
-# B. both_ends
-# Given a string s, return a string made of the first 2
-# and the last 2 chars of the original string,
-# so 'spring' yields 'spng'. However, if the string length
-# is less than 2, return instead the empty string.
-def both_ends(s):
+# B. front_x
+# Given a list of strings, return a list with the strings
+# in sorted order, except group all the strings that begin with 'x' first.
+# e.g. ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
+# ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
+# Hint: this can be done by making 2 lists and sorting each of them
+# before combining them.
+def front_x(words):
   # +++your code here+++
-  empty = ''
-  if len(s) < 2:
-    return empty
-  else:
-    return s[:2] + s[-2:]
-	
+  return
 
-# C. fix_start
-# Given a string s, return a string
-# where all occurences of its first char have
-# been changed to '*', except do not change
-# the first char itself.
-# e.g. 'babble' yields 'ba**le'
-# Assume that the string is length 1 or more.
-# Hint: s.replace(stra, strb) returns a version of string s
-# where all instances of stra have been replaced by strb.
-def fix_start(s):
+
+
+# C. sort_last
+# Given a list of non-empty tuples, return a list sorted in increasing
+# order by the last element in each tuple.
+# e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
+# [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
+# Hint: use a custom key= function to extract the last element form each tuple.
+def sort_last(tuples):
   # +++your code here+++
-  first_letter = s[0]
-  rest_s = s[1:]
-  return first_letter + rest_s.replace(first_letter, '*')
+  return
 
 
-# D. MixUp
-# Given strings a and b, return a single string with a and b separated
-# by a space '<a> <b>', except swap the first 2 chars of each string.
-# e.g.
-#   'mix', pod' -> 'pox mid'
-#   'dog', 'dinner' -> 'dig donner'
-# Assume a and b are length 2 or more.
-def mix_up(a, b):
-  # +++your code here+++
-  return b[0:2] + a[2:] + ' ' + a[0:2] + b[2:]
-
-
-# Provided simple test() function used in main() to print
+# Simple provided test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
 def test(got, expected):
   if got == expected:
     prefix = ' OK '
   else:
     prefix = '  X '
-  print ('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
 
 
-# Provided main() calls the above functions with interesting inputs,
-# using test() to check if each result is correct or not.
+# Calls the above functions with interesting inputs.
 def main():
-  print ('donuts')
-  # Each line calls donuts, compares its result to the expected for that call.
-  test(donuts(4), 'Number of donuts: 4')
-  test(donuts(9), 'Number of donuts: 9')
-  test(donuts(10), 'Number of donuts: many')
-  test(donuts(99), 'Number of donuts: many')
+  print 'match_ends'
+  test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
+  test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
+  test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
 
   print
-  print ('both_ends')
-  test(both_ends('spring'), 'spng')
-  test(both_ends('Hello'), 'Helo')
-  test(both_ends('a'), '')
-  test(both_ends('xyz'), 'xyyz')
+  print 'front_x'
+  test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
+       ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
+  test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
+       ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
+  test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
+       ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
 
-  
+       
   print
-  print ('fix_start')
-  test(fix_start('babble'), 'ba**le')
-  test(fix_start('aardvark'), 'a*rdv*rk')
-  test(fix_start('google'), 'goo*le')
-  test(fix_start('donut'), 'donut')
-
-  print
-  print ('mix_up')
-  test(mix_up('mix', 'pod'), 'pox mid')
-  test(mix_up('dog', 'dinner'), 'dig donner')
-  test(mix_up('gnash', 'sport'), 'spash gnort')
-  test(mix_up('pezzy', 'firm'), 'fizzy perm')
+  print 'sort_last'
+  test(sort_last([(1, 3), (3, 2), (2, 1)]),
+       [(2, 1), (3, 2), (1, 3)])
+  test(sort_last([(2, 3), (1, 2), (3, 1)]),
+       [(3, 1), (1, 2), (2, 3)])
+  test(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]),
+       [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
 
 
-# Standard boilerplate to call the main() function.
 if __name__ == '__main__':
   main()
