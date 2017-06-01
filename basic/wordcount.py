0c68d8858fcad4efa26b2e556467fb59e8531c03
vwon23 <vwon87@gmail.com>
2017-05-31 23:30:46 -0700

    inprogress

--- basic/wordcount.py
+++ basic/wordcount.py
@@ -44,8 +44,34 @@ import sys
 # You could write a helper utility function that reads a file
 # and builds and returns a word/count dict for it.
 # Then print_words() and print_top() can just call the utility function.
-
+def word_count_dict(filename):
+  word_count = {}
+  input_file = open(filename, 'r')
+  for line in input_file:
+    words = line.split()
+    for word in words:
+      word = word.lower()
+      if word in word_count:
+        word_count[word] += 1
+      else:
+        word_count[word] = 1
+  input_file.close()
+  return word_count
+  
+def print_words(filename):
+  word_count = word_count_dict(filename)
+  words = sorted(word_count.keys())
+  for key in words:
+    print (key, word_count[key])
+ 
+option = sys.argv[1]
+filename = sys.argv[2]
+print (print_words(filename)) 
 ###
+#def print_top(filename):
+  
+    
+
 
 # This basic command line argument parsing code is provided and
 # calls the print_words() and print_top() functions which you must define.
--- hello.py
+++ hello.py
@@ -19,14 +19,16 @@ and run Python code; now you just need to learn Python!
 
 import sys
 
+def cat(filename):
+  f = open(filename, 'rU')
+  text = f.read()
+  print (text,)  
+  f.close()
+  
 # Define a main() function that prints a little greeting.
 def main():
   # Get the name from the command line, using 'World' as a fallback.
-  if len(sys.argv) >= 2:
-    name = sys.argv[1]
-  else:
-    name = 'World'
-  print ('Hello', name)
+  cat(sys.argv[1])
 
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
